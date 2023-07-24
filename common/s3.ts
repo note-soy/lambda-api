9325815361310076adef7c33e80fc3f83adbe6a9
EJ <e@j>
2023-07-24 09:48:54 -0400

    get sample note

---
+++ common/s3.ts
@@ -0,0 +1,17 @@
+import {GetObjectCommand, S3Client} from "@aws-sdk/client-s3";
+
+const REGION = "us-east-1";
+const s3Client = new S3Client({ region: REGION });
+
+const BUCKET = "note.soy-inventory";
+
+export const get_note = async (key: string) => {
+
+    const results = await s3Client.send(new GetObjectCommand({
+        Bucket: BUCKET,
+        Key: key,
+    }));
+
+    return await results.Body.transformToString();
+
+}
--- functions/get_note/index.ts
+++ functions/get_note/index.ts
@@ -1,12 +1,12 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import {get_note} from "../../common/s3";
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+
   return {
       statusCode: 200,
-      body: JSON.stringify({
-          message: 'hello world',
-      }),
+      body: await get_note("test.txt"),
    };
 };
